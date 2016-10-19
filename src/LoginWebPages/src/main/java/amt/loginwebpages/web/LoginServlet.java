2b9d2df121e9b9374f673a35da97c6d56834a5e7
Ciani Antony <antony.ciani@heig-vd.ch>
2016-10-19 13:35:53 +0200

    Cleaning

--- src/LoginWebPages/src/main/java/amt/loginwebpages/web/LoginServlet.java
+++ src/LoginWebPages/src/main/java/amt/loginwebpages/web/LoginServlet.java
@@ -30,7 +30,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
         //request.setAttribute("users", lm.findAllUsers());
         //request.getRequestDispatcher("WEB-INF/pages/users.jsp").forward(request, response);
         
